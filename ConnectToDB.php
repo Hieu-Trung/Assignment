f8c48738a26c33668289a5df8ead10996b3bd4b4
Hieu-Trung <trungthgcd17062@fpt.edu.vn>
2019-06-25 22:35:52 +0700

    Update ConnectToDB.php

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -36,7 +36,7 @@ $stmt = $pdo->prepare($sql);
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
 $stmt->execute();
 $resultSet = $stmt->fetchAll();
-echo '<p>Students information:</p>';
+echo '<p>User information:</p>';
 foreach ($resultSet as $row) {
 	echo $row['stuid'];
         echo "    ";
