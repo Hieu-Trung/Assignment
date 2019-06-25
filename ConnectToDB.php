f30d2849f7c78d3aec8910b053a6314555e402a1
Hieu-Trung <trungthgcd17062@fpt.edu.vn>
2019-06-25 17:07:00 +0700

    Update ConnectToDB.php

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -39,7 +39,7 @@ $resultSet = $stmt->fetchAll();
 echo '<p>Students information:</p>';
 foreach ($resultSet as $row) {
 	echo $row['stuid'];
-        echo "    ";<br>
+        echo "    ";
         echo $row['fname'];
         echo "    ";
         echo $row['email'];
