1580fc5733d10bba8a1796eacada7d770e851559
Hieu-Trung <trungthgcd17062@fpt.edu.vn>
2019-06-25 17:14:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,13 +21,13 @@
  	<br>
  	<br>
  	<br>
-     <a href="ConnectToDB.php" target="_blank" style="border: 1px solid yellow; float: left; margin-left: 300px; margin-top: 55px; text-decoration: none; color: red;" >Connect database</a>
+     <a href="ConnectToDB.php" target="_blank" style="border: 1px solid black; float: left; margin-left: 300px; margin-top: 55px; text-decoration: none; color: black; background: white;" >Connect database</a>
      <br/>
-     <a href="InsertData.php" target="_blank" style="border: 1px solid; float: left; margin-left: 50px; margin-top: 36px; text-decoration: none;">Insert data to the database</a>
+     <a href="InsertData.php" target="_blank" style="border: 1px solid black; float: left; margin-left: 50px; margin-top: 36px; text-decoration: none; color: black; background: white;">Insert data to the database</a>
      <br/>
-     <a href="UpdateData.php" target="_blank" style="border: 1px solid; float: left; margin-left: 50px; margin-top: 18px; text-decoration: none;">Update data to the database</a> 
+     <a href="UpdateData.php" target="_blank" style="border: 1px solid black; float: left; margin-left: 50px; margin-top: 18px; text-decoration: none; color: black; background: white;">Update data to the database</a> 
     <br/>
-    <a href="DeleteData.php" target="_blank" style="border: 1px solid; float: left; margin-left: 50px; text-decoration: none;">Delete data to the database</a> 
+    <a href="DeleteData.php" target="_blank" style="border: 1px solid black; float: left; margin-left: 50px; text-decoration: none; color: black; background: white;">Delete data to the database</a> 
 
  </body>
 </html>
